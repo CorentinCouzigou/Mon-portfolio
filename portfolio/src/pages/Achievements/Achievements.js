d327470686cb89c6966da585f7a35b03809c5bf0
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2021-11-17 11:55:43 +0100

    scrollbar inprogress

--- portfolio/src/pages/Achievements/Achievements.js
+++ portfolio/src/pages/Achievements/Achievements.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
 import "./style.scss";
 
@@ -11,7 +12,31 @@ function Achievements() {
                         <span className="achievements__presentation__span">Mes réalisations</span>
                     </div>
                 </div>
+                <div className="achievements__cards__container">
+                    <div className="card">
 
+                        <div className="card__overlay">
+
+                        </div>
+                    </div>
+                    <div className="card">
+
+                        <div className="card__overlay">
+
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card__overlay">
+                            <a href="https://drive.google.com/file/d/1yKCGihIOdV57gnlghPC1SP4WcPDF2ZHp/view?usp=sharing"><p>hello word</p></a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="overlay"></div>
+
+                    </div>
+
+
+                </div>
 
             </div>
         </div>
