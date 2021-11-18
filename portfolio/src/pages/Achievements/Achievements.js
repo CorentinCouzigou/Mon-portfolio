6b2c1941b8adb9431f5f2fa1c6443ce67560b0da
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2021-11-18 10:14:42 +0100

    sort achivements check

--- portfolio/src/pages/Achievements/Achievements.js
+++ portfolio/src/pages/Achievements/Achievements.js
@@ -1,9 +1,11 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React from 'react';
-import "./style.scss";
+import React, { useState } from 'react';
+import "./achievements.scss";
 import { achievementsData } from './achievementsData';
 
 function Achievements() {
+    const [achievements, setAchievements] = useState(achievementsData);
+    console.log(achievements.length);
     return (
         <div className="achievements">
             <div className="achievements__container">
@@ -13,24 +15,28 @@ function Achievements() {
                         <span className="achievements__presentation__span">Mes réalisations</span>
                     </div>
                 </div>
+                <ul className="liste">
+                    <li className={achievements.length === achievementsData.length ? "select" : ""} onClick={() => setAchievements(achievementsData)}>All</li>
+                    <li className={achievements.every(element => element.nature === "front") && "select"} onClick={() => setAchievements(achievementsData.filter(element => element.nature === "front"))}>Front</li>
+                    <li className={achievements.every(element => element.nature === "back") && "select"} onClick={() => setAchievements(achievementsData.filter(element => element.nature === "back"))}>Back
+                    </li>
+                </ul>
                 <div className="achievements__cards__container">
-                    {achievementsData.map((item) => {
+                    {achievements.map((item) => {
                         return (
                             <div className="card">
-                                <img src={item.image} alt="photo" />
-
+                                <img className="card__img" src={item.image} alt="photo" />
                                 <a className="card__overlay" href={item.link}><p>
                                     {item.title}<br />
                                     {item.desc}
                                 </p></a>
-
                             </div>
                         )
                     })}
                 </div>
 
             </div>
-        </div>
+        </div >
     )
 }
 
--- portfolio/src/pages/Achievements/achievementsData.js
+++ portfolio/src/pages/Achievements/achievementsData.js
@@ -3,28 +3,32 @@ import React from 'react';
 
 export const achievementsData = [
     {
-        title: 'Github',
+        title: 'Github1',
         desc: `salut cest coco`,
         image: "https://cdn.pixabay.com/photo/2021/11/11/13/08/leopard-6786267_960_720.jpg",
         link: "https://github.com/CorentinCouzigou",
+        nature: "front",
     },
     {
-        title: 'Github',
+        title: 'Github2',
         desc: `salut cest coco`,
         image: "https://cdn.pixabay.com/photo/2021/11/11/13/08/leopard-6786267_960_720.jpg",
         link: "https://github.com/CorentinCouzigou",
+        nature: "front"
     },
     {
-        title: 'Github',
+        title: 'Github3',
         desc: `salut cest coco`,
         image: "https://cdn.pixabay.com/photo/2021/11/11/13/08/leopard-6786267_960_720.jpg",
         link: "https://github.com/CorentinCouzigou",
+        nature: "back",
     },
     {
-        title: 'Github',
+        title: 'Github4',
         desc: `salut cest coco`,
         image: "https://cdn.pixabay.com/photo/2021/11/11/13/08/leopard-6786267_960_720.jpg",
         link: "https://github.com/CorentinCouzigou",
+        nature: "back",
     },
 
 ];
