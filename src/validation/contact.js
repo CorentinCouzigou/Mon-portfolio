8672caf19663bd5e6c25a90be218f96a8b1a334d
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2021-12-15 10:15:31 +0100

    changement of regex in my page contact CHECK

--- src/validation/contact.js
+++ src/validation/contact.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 export const schema = Joi.object({
     nom: Joi.string().pattern(new RegExp('^[^(<(\w)>(.*)</(\w)>)]+$')).min(1).max(30).required().required(),
-    email: Joi.string().pattern(new RegExp('^[A-Z0-9a-z._%+-]+@[a-z0-9.-]+\\.[a-z]{1,4}}*$')).email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr'] } }).required(),
-    message: Joi.string().pattern(new RegExp('^[A-Za-zéè ,;!:-?@^.\\n]*$')).min(1).max(200).required(),
+    email: Joi.string().pattern(new RegExp('^[A-Z0-9a-z._%+-]+@[a-z0-9.-]+\\.[a-z]{1,4}}*$')).email({ tlds: { allow: false } }).required(),
+    message: Joi.string().pattern(new RegExp('^[A-Za-zéèÏïÉç ,;!:-?@^.\\n]*$')).min(1).max(200).required(),
 });
