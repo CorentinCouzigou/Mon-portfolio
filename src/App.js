65d61a14c593559859e050eca00c1ede7b5ec027
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2022-02-09 12:55:15 +0100

    in progress deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-    {/* A Décommenter pour que la navbar soit active Important!! */}
+    {/* A Décommenter pour que la navbar soit active Important!!§ */}
       {/* <Navbar /> */}
       <ParticlesComponent />
       <Routes>
